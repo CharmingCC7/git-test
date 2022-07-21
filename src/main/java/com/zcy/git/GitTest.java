6ae0feb078bf1c25b2d14503fac10874eb44be5f
CharmCC <CharmCC@ZCY.com>
2022-07-21 20:19:26 +0800

    first commit

---
+++ src/main/java/com/zcy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zcy.git;
+
+/**
+ * @author 冠军
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
